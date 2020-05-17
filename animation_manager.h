3c004560b2d81025f45e3b840ec883e4c041e648
Nelson Penn <nelsonapenn@gmail.com>
2020-05-16 23:07:50 -0400

    more skeleton stuff

---
+++ animation_manager.h
@@ -0,0 +1,39 @@
+#ifndef ANIMATION_MANAGER_H
+#define ANIMATION_MANAGER_H
+enum ObjectTypes
+{
+  GRASS,
+  ROCK,
+  WALL,
+  BETH
+};
+enum Region
+{
+  DESERT,
+  VOLCANO,
+  GRASSLANDS
+};
+enum AnimationTypes
+{
+  IDLE,
+  DIE,
+  BREAK,
+  UP_STRIKE,
+  UP_SWING,
+  DOWN_STRIKE,
+  DOWN_SWING,
+  FORWARD_STRIKE,
+  FORWARD_SWING,
+  BACK_STRIKE,
+  BACK_SWING,
+  FORWARD_WALK,
+  DODGE,
+  PARRY,
+  BLOCK,
+  JUMP
+};
+static class AnimationManager
+{
+
+};
+#endif
--- object.h
+++ object.h
@@ -1,1 +1,6 @@
 // An object that has interactions with others in the physics engine
+class Object
+{
+  bool mobile;
+  
+};
